95da57cd6f5cbc539de41af9ba4aa88dea3fea4c
KnowingWithShrish <89601497+KnowingWithShrish@users.noreply.github.com>
2021-11-03 23:15:58 +0530

    read() readline() functions

---
+++ fileIO.py
@@ -0,0 +1,17 @@
+f=open("shrish.txt", "rt")
+
+print(f.readline(),end="")
+print(f.readline(),end="")
+
+# content = f.read()
+
+# for line in f :
+#     print(line,end="")
+
+# print(content)
+# content = f.read(3)
+# print(content)
+# content = f.read(3)
+# print(content)
+
+f.close()
