a3b55727a05a6f118daf82d2f1b5835ab472d97c
KnowingWithShrish <89601497+KnowingWithShrish@users.noreply.github.com>
2021-11-03 10:19:35 +0530

    Guess te number in 9 gueses

---
+++ guessTheNumber.py
@@ -0,0 +1,23 @@
+import random
+n =random.randint(1,100)
+print("You have total 9 guesses")
+
+no_of_guesses =9
+
+while no_of_guesses>=1:
+    no_of_guesses-=1
+    x= int(input("Enter Your Guess\n"))
+    if x<n:
+        print("Your number is too less!")
+        print(f'you have {no_of_guesses} guesses left')
+    elif x>n:
+        print("Your number is too high!")
+        print(f'you have {no_of_guesses} guesses left')
+    else :
+        print("You have guessed the right number in {no_of_guesses} guesses\nYou Won!")
+        break
+
+    if no_of_guesses == 0:
+        print("Game Over!")
+    
+    
