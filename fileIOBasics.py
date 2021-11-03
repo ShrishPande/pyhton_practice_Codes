409112da2d4d1500b27c371566dba303e4aece27
KnowingWithShrish <89601497+KnowingWithShrish@users.noreply.github.com>
2021-11-03 17:54:34 +0530

    adding fileIOBasics file

---
+++ fileIOBasics.py
@@ -0,0 +1,10 @@
+#File IO Basics
+"""
+"r" - OPen file for reading
+"w" - Open file for wriing
+"x" - Creates file if not exists
+"a" - Add more content to a file
+"t" - text mode
+"b" - binary mode
+"+" - read and write both 
+"""
