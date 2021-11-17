93903b0556189991d8469df49592666445ad2593
KnowingWithShrish <89601497+KnowingWithShrish@users.noreply.github.com>
2021-11-17 12:51:15 +0530

    Python pattern

--- pattern5.py
+++ pattern5.py
@@ -2,6 +2,9 @@ num = int(input("Enter Your Number"))
 for i in range(1,num+1):
     for j in range(i,num):
         print(" ",end="")
-    for k in range(2*i-1):
-        print("*",end="")
+    for k in range(1,2*i):
+        if k==1 or k==2*i-1 or i==(num//2)+1:
+            print("*",end="")
+        else:
+            print(" ",end="")
     print()
